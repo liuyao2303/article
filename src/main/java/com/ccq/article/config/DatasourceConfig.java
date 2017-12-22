d17b1a9f41a6f118c1aaa8b8c4abaa04da2067ab
liuyao <18252063065@163.com>
2017-12-22 16:52:16 +0800

    数据源学习demo

---
+++ src/main/java/com/ccq/article/config/DatasourceConfig.java
@@ -0,0 +1,27 @@
+package com.ccq.article.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DatasourceConfig {
+
+    @Bean("hikariDataSource")
+    public HikariDataSource hikariDataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:mysql://kk.hb520.site:3306/ccq");
+        config.setUsername("root");
+        config.setPassword("lyhh2303");
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+        config.setIdleTimeout(60000);
+        config.setConnectionTimeout(60000);
+        config.setValidationTimeout(3000);
+        config.setMaxLifetime(60000);
+        HikariDataSource ds = new HikariDataSource(config);
+        return ds;
+    }
+}
---
+++ src/main/java/com/ccq/article/config/JdbcConfig.java
@@ -0,0 +1,17 @@
+package com.ccq.article.config;
+
+import com.ccq.framework.jdbc.dao.JdbcTempleteDao;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+
+@Component
+public class JdbcConfig {
+
+    @Bean
+    JdbcTempleteDao getJdbcTemplate(HikariDataSource ds) {
+        return new JdbcTempleteDao(ds);
+    }
+}
